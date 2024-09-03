ca77fdd0177c0fe26a930e9c0b2457ef47575079
Ethan Barron <83491820+EthanJamesBarron@users.noreply.github.com>
2024-09-03 07:49:13 +0100

    Util now has a static SendMessage function

--- GangBeastsIL2CPPDevelopment/Utils.cs
+++ GangBeastsIL2CPPDevelopment/Utils.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using CoreNet.Messaging.Messages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace BeastNet
 {
     public static class Utils
     {
+        public static void SendMessage(short messageCode, MessageBase message, bool serverIsSendingThis = true, bool reliable = true)
+        {
+            if (message is null)
+            {
+                Debug.LogError("Message passed to SendMessage is null!");
+                return;
+            }
+
+            if (serverIsSendingThis)
+            {
+                if (NetworkServer.active)
+                {
+                    NetworkServer.SendByChannelToAll(messageCode, message, reliable ? 0 : 1);
+                    return;
+                }
+
+                Debug.LogError("SendMessageToAll was meant to be used on the server but has been used on the client!");
+                return;
+            }
+
+            if (NetworkClient.active) NetworkManager.singleton.client.SendByChannel(messageCode, message, reliable ? 0 : 1);
+        }
     }
 }
