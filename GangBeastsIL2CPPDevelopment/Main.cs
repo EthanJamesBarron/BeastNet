acbf0461551393ebb90688f8b75c3392fe66cf3e
Ethan Barron <83491820+EthanJamesBarron@users.noreply.github.com>
2024-09-03 07:19:35 +0100

    Add project files.

---
+++ GangBeastsIL2CPPDevelopment/Main.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using Coatsink.UnityServices;
+using GB.Data;
+using HarmonyLib;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GangBeastsIL2CPPDevelopment
+{
+    [BepInPlugin("com.ethanbarron.il2cppmodding", "GangBeastsIL2CPPDevelopment", "0.0.1")]
+    public class Main : BasePlugin
+    {
+        public override void Load()
+        {
+            AddComponent<Core>();
+
+            new Harmony("Cheese").PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+            SceneManager.add_sceneLoaded(new Action<Scene, LoadSceneMode>(WrapperFix));
+        }
+
+        public void WrapperFix(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "Menu")
+            {
+                AddComponent<DevelopmentTestServer>().ui = GameObject.Find("Global(Clone)/UI/PlatformUI/Development Server Menu").GetComponent<DevelopmentTestServerUI>();
+
+                if (Environment.GetCommandLineArgs().Contains("-SERVER"))
+                {
+                    UnityServicesManager.Instance.Initialise(UnityServicesManager.InitialiseFlags.DedicatedServer, null, "", "DGS");
+                    AudioListener.pause = true;
+                    GameObject.Find("Global(Clone)/LevelLoadSystem/ScreenDisplay").SetActive(false);
+                }
+            }
+        }
+    }
+
+    public class Core : MonoBehaviour
+    {
+
+    }
+
+    [HarmonyPatch(typeof(DevelopmentTestServer), nameof(DevelopmentTestServer.Awake))]
+    public class CommandLineFix
+    {
+        public static void Postfix()
+        {
+            string valueForKey = CommandLineParser.Instance.GetValueForKey("-DDC_IP", true);
+            string valueForKey2 = CommandLineParser.Instance.GetValueForKey("-DDC_PORT", true);
+            if (!string.IsNullOrEmpty(valueForKey2))
+            {
+                DevelopmentTestServer.DirectConnectPort = int.Parse(valueForKey2);
+            }
+            if (!string.IsNullOrEmpty(valueForKey))
+            {
+                DevelopmentTestServer.DirectConnectIP = valueForKey;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(StringLoader), nameof(StringLoader.LoadString))]
+    public static class StringTester
+    {
+        public static void Postfix(ref string key)
+        {
+            Debug.LogError("LoadString called with a key of " + key);
+        }
+    }
+}
